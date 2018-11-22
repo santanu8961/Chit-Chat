d57cce9947c478f34eecb6450e09cc72bd619a84
santanu <edu.santanu12@gmail.com>
2018-11-23 01:21:51 +0530

    2

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname,'../public');
 
 app.use(express.static(publicPath));
 
-const port = 3000;  
+const port =  process.env.PORT || 3000 ;
 app.listen(port,()=>{
     console.log(`Server is running at ${port}`);
 })
