d5d1b979d65dd7b0aaa9ca08afec73f284eea6f0
santanu <edu.santanu12@gmail.com>
2018-11-23 22:56:23 +0530

    connect disconnected feature added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 
 app.use(express.static(publicPath));
 
+const server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('New User Connected..')
+    socket.on('disconnect',()=>{
+        console.log('user Disconnected');
+    })
+})
+
+
 const port =  process.env.PORT || 3000 ;
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is running at ${port}`);
 })
 
